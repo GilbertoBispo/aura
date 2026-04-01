51e84379fa48d8e67f4d6fb741786f76dd1383e1
Gilberto <gil79430@gmail.com>
2026-04-01 12:54:46 -0300

    feat: cria uma página de criação de post

--- frontend/src/components/CriarPost/CriarPost.jsx
+++ frontend/src/components/CriarPost/CriarPost.jsx
@@ -1,19 +1,25 @@
-import { Image } from "lucide-react";
+import { Image, Laugh, AtSign } from "lucide-react";
 
 function CriarPost() {
     return (
         <section className="bg-amber-50 h-screen flex flex-col items-center">
             
-            <form action="" className="bg-amber-300 w-full h-[50%] flex gap-2.5 p-[10px_0_0_0] flex-col">
-                <div className="flex gap-1.25 h-fit w-fit">
-                    <a className="rounded-[100px]" href="">
+            <form action="" className="bg-(--primary-color) w-full h-[50%] flex gap-10 p-[10px_0_0_0] flex-col">
+                <div className="flex h-fit w-full justify-evenly">
+                    <a className="rounded-[100px] w-fit h-fit" href="">
                         <div className="w-10 h-10 bg-(--secondary-color) rounded-[100px]"></div>
                     </a>
-                    <textarea type="text" className="bg-amber-600 w-[80%] h-28"/>
+                    <textarea type="text" className="bg-(--secondary-color) w-[80%] h-28 rounded-[5px] text-white focus:outline-none p-2.5"/>
                 </div>  
-                <div className="w-fit h-fit">
-                    <Image></Image>
-                    
+                <div className="w-[80%] h-fit flex self-end pr-3.5 justify-between">
+                    <div className="flex gap-10">
+                        <Image className="text-white"></Image>
+                        <Laugh className="text-white"></Laugh>
+                        <AtSign className="text-white"></AtSign>
+                    </div>
+                    <div>
+                        <button className="text-black p-2.5 bg-white rounded-[5px]">Post</button>
+                    </div>
                 </div>
             </form>
         </section>
